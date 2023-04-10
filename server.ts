96603b1ae06c49ba0f3f95f6071360ca6eba4d16
henriquevenchiarutti <82562877+henriquevenchiarutti@users.noreply.github.com>
2023-04-10 03:55:04 -0300

    Finishing simple APIs to manage products, create, get all and mass delete

--- server.ts
+++ server.ts
@@ -1,3 +1,14 @@
-import App from './src/App';
+import express from 'express';
+import mongoose from 'mongoose';
+import ProductRouter from './src/controller/productRouter';
 
-App.start();
+mongoose.connect('mongodb://localhost:27017/products-management');
+
+const app = express();
+const productRouter = new ProductRouter().getRouter();
+
+app.use('/products', productRouter);
+
+app.listen(3000, () => {
+  console.log('Server started on port 3000');
+});
